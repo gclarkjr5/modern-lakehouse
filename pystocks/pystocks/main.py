443a7bfa03649df0360e7e8b8108f6024a7918e4
Gary Clark <gclark@xccelerated.io>
2022-10-27 13:21:08 +0200

    dagster basic setup
    getting stocks, and saving stocks with dependency showing up in graph and ability to materialiaze over and over and see data added to the csv

--- pystocks/pystocks/main.py
+++ pystocks/pystocks/main.py
@@ -1,32 +1,52 @@
 import os
-import re
 from datetime import datetime, timedelta, timezone
 import yfinance as yf
+from dagster import asset
 
 
+@asset
+def stock_data():
+    yesterday = datetime.now()
+    yesterday = yesterday.replace(tzinfo=timezone.utc)
+    yesterday = yesterday - timedelta(1)
+
+    two_days_ago = yesterday - timedelta(1)
+
+    start_date = datetime.strftime(two_days_ago, '%Y-%m-%d')
+    end_date = datetime.strftime(yesterday, '%Y-%m-%d')
+
+    obj = yf.Ticker('goog')
+    df = obj.history(start=start_date, end=end_date, tz='UTC')
+
+    return df
 
 
-yesterday = datetime.now()
-yesterday = yesterday.replace(tzinfo=timezone.utc)
-yesterday = yesterday - timedelta(1)
+@asset
+def save_stock_data_op(stock_data):
+    if os.path.exists('google.csv'):
+        stock_data.to_csv('google.csv', mode='a', header=False, index=True)
+    else:
+        stock_data.to_csv('google.csv', mode='a', index=True)
 
-two_days_ago = yesterday - timedelta(1)
+    # target_file = context.op_config["target_file"]
+    # symbols = context.op_config["symbols"]
+    # start_date  = context.op_config["start_date"]
+    # if not re.match(start_date, "^[0-9]{4}-[0-9]{2}-[0-9]{2}$"):
+    #     start_date = '2000-01-01'
+    # end_date = context.op_config["end_date"]
+    # if not re.match(end_date, "^[0-9]{4}-[0-9]{2}-[0-9]{2}$"):
+    #     end_date = yesterday(start_date)
 
-start_date = datetime.strftime(two_days_ago, '%Y-%m-%d')
-end_date = datetime.strftime(yesterday, '%Y-%m-%d')
+    # if os.path.exists(target_file):
+    #     os.remove(target_file)
 
-obj = yf.Ticker('goog')
-df = obj.history(start=start_date, end=end_date, tz='UTC')
+    # for symbol in symbols:
+    #     ticker = yf.Ticker(symbol)
+    #     df = ticker.history(start=start_date, end=end_date)
+    #     df.insert(0, 'Symbol', symbol)
+    #     df.to_csv(target_file, mode='a', header=False, index=True)
 
-if os.path.exists('google.csv'):
-    df.to_csv('google.csv', mode='a', header=False, index=True)
-else:
-    df.to_csv('google.csv', mode='a', index=True)
-
-
-
-
-# from dagster import job, op
+    # return target_file
 
 # def yesterday(date:str) -> str:
 #     yesterday = datetime.now() - timedelta(1)
